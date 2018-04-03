a6928e05742eef3ef5683af371141850a0c937b5
Richard Kim <richeekim@gmail.com>
2018-04-03 01:04:09 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,11 @@
 def my_select(collection)
-   i = 0
-   arr = []
-   while i < collection.length
-     arr << yield(collection[i])
-     i += 1
-   end
-   return arr
+  #  i = 0
+  #  arr = []
+  #  while i < collection.length
+  #    if
+  #    arr << yield(collection[i])
+  #    i += 1
+  #  end
+  #  return arr
+  return collection
 end
